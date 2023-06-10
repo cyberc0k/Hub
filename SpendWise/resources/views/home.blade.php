400027bf72a74c2b5118aff92d065ac5adeb4f5f
cyberc0k <cyberrcok@gmail.com>
2023-06-10 16:25:11 +0300

    Профиль
    Добавлнеие шрифтов, главное изображение,

--- SpendWise/resources/views/home.blade.php
+++ SpendWise/resources/views/home.blade.php
@@ -2,22 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    {{--        // колличество колнок и отступ--}}
+    <div class="col-9 pt-5" >
+        <img src="https://sun1-19.userapi.com/impg/c855124/v855124303/21d669/-5ak96MrdIQ.jpg?size=2160x2160&quality=96&sign=a3a77f302e1f07cf9161da1cff5ca23e&type=album" style="height: 300px" class="rounded-circle">
+        <div>
+            <h1>            Анатолий Соловьёв
+            </h1>
+            {{--            //располжение в одну строку--}}
         </div>
+        <div class="d-flex">
+            <div><strong>153</strong> posts</div>
+            <div><strong>23k</strong> followers</div>
+            <div><strong>212</strong> following</div>
+        </div>     </div>
     </div>
-</div>
 @endsection
