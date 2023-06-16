35f6654a625b337a9a669a4ac58462ebff6dc174
cyberc0k <cyberrcok@gmail.com>
2023-06-16 21:39:36 +0300

    связывание таблицы = добавление идендификатора

--- SpendWise/database/migrations/2023_06_16_182741_create_profiles_table.php
+++ SpendWise/database/migrations/2023_06_16_182741_create_profiles_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('title')->nullable();//необязательные поля
             $table->text("description")->nullable();
             $table->string('url')->nullable();
